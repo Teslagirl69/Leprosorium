d1eca73e41677a72925c300f90a22539a3e7ac7f
Teslagirl69 <teslagirl17@gmail.com>
2022-05-16 15:56:35 +0300

    Add stab for new and main

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
+
+get '/new' do
+	erb "Hello world"
+end
+
+
+
+get '/main' do
+	erb "Hello world"
+end
+
